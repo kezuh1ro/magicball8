9f224fa5c8131534159027c9b9dc7a92718b0faa
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-08-26 10:48:52 +0500

    Update magicball8.py

--- magicball8.py
+++ magicball8.py
@@ -1,3 +1,16 @@
 import telebot
 import random
 
+bot = telebot.TeleBot('6457055794:AAHObLLgjZRVy7jUB3e_I0LFYa_yZNFA9zU')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+  bot.send_message(message.chat.id, f"<b>Привет</b> {message.from_user.first_name}<b>!</b>", parse_mode='html')
+
+@bot.message_handler(commands='ball'])
+def ball(message):
+  bot.send_message(message.chat.id, f"<b>Задай мне вопрос.</b>", parse_mode='html')
+
+@bot.message_handler(content_types=['text'])
+def question(message):
